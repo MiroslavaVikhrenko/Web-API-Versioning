627bd9feaf3701178f7d7f2c20631a5c572b8f73
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-18 15:18:19 -0700

    Add CountriesController class with Get() action method

---
+++ Web-API-Versioning/Controllers/CountriesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web_API_Versioning.Models.DTOs;
+
+namespace Web_API_Versioning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var countriesDomainModel = CountriesData.Get();
+
+            //Map Domain Model to DTO
+            var response = new List<CountryDto>();
+
+            foreach (var countryDomain in countriesDomainModel)
+            {
+                response.Add(new CountryDto
+                {
+                    Id = countryDomain.Id,
+                    Name = countryDomain.Name,
+                });
+            }
+
+            return Ok(response);
+        }
+    }
+}
